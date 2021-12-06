61704e0a2bafe2f8a07681fef5224ebc0007b002
gpark31 <55361125+gpark31@users.noreply.github.com>
2021-12-05 21:19:21 -0500

    level2Completed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,17 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayGame(int l)
+    {
+        SceneManager.LoadScene(l);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void ReturnToMenu ()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
-    public void QuitGame ()
+    public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
